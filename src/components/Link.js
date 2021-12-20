12080e47578ffc1f4bf886c8e50500cd512cddd0
buckyhelmsmash <buckyhelmsmash@gmail.com>
2021-12-20 15:57:41 +0700

    Refactor Link.js

--- src/components/Link.js
+++ src/components/Link.js
@@ -33,23 +33,23 @@ const Link = ({index, link}) => {
     }, [state.fetching, executeMutation, link.id]);
 
     return (<div className="flex mt2 items-start">
-            <div className="flex items-center">
-                <span className="gray">{index + 1}.</span>
-                {isLoggedIn && (<button className="button" onClick={upvote}>
-                         ⬆️ Vote
-                    </button>)}
+        <div className="flex items-center">
+            <span className="gray">{index + 1}.</span>
+            {isLoggedIn && (<button className="button" onClick={upvote}>
+                ⬆️ Vote
+            </button>)}
+        </div>
+        <div className="ml1">
+            <div>
+                {link.description} ({link.url})
             </div>
-            <div className="ml1">
-                <div>
-                    {link.description} ({link.url})
-                </div>
-                <div className="f6 lh-copy gray">
-                    {link.votes.length} votes | by{' '}
-                    {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
-                    {timeDifferenceForDate(link.createdAt)}
-                </div>
+            <div className="f6 lh-copy gray">
+                {link.votes.length} votes | by{' '}
+                {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+                {timeDifferenceForDate(link.createdAt)}
             </div>
-        </div>)
+        </div>
+    </div>)
 }
 
 export default Link
