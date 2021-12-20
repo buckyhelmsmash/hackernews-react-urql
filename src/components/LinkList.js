fc09a9ba85a783838d3ceb5059c651c4d60a625d
buckyhelmsmash <buckyhelmsmash@gmail.com>
2021-12-20 11:36:58 +0700

    Endpoint handshake

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -16,27 +16,20 @@ const FEED_QUERY = gql`
   }
 `
 
-const linksToRender = [
-    {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎',
-        url: 'https://www.prismagraphql.com',
-    },
-    {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://formidable.com/open-source/urql/',
-    },
-]
-
 const LinkList = () => {
-    useQuery({query: FEED_QUERY});
+    const [result] = useQuery({ query: FEED_QUERY })
+    const { data, fetching, error } = result
+
+    if (fetching) return <div>Fetching</div>
+    if (error) return <div>Error</div>
+
+    const linksToRender = data.feed.links
 
     return (
         <div>
-            {linksToRender.map(link => <Link key={link.id} link={link}/>)}
+            {linksToRender.map(link => <Link key={link.id} link={link} />)}
         </div>
-    );
-};
+    )
+}
 
 export default LinkList
